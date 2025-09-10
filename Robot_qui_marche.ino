315a93b05f96c476b1f5b4949177a576919a6113
AmauSup <amaury.demaille@supdevinci-edu.fr>
2025-09-10 15:18:37 +0200

    Premier commit

--- Robot_qui_marche.ino
+++ Robot_qui_marche.ino
@@ -1,102 +1,99 @@
-#include <Servo.h>
+#include <Servo.h> // Bibliothèque pour contrôler les servomoteurs
 
-// ----------------- Definition des fonctions
+// ----------------- Définition des broches pour le moteur -----------------
+#define EN 3  // Broche d'activation (Enable) du moteur
+#define IN1 5 // Broches de contrôle moteur 1
+#define IN2 7
+#define IN3 6 // Broches de contrôle moteur 2
+#define IN4 8
 
-#define EN 3 
-#define IN1 5 
-#define IN2 7 
-#define IN3 6 
-#define IN4 8 
+// ----------------- Définition des constantes -----------------
+#define speed_Max 100    // Vitesse maximale du moteur
+#define Turn90deg 500    // Durée approximative pour tourner à 90 degrés
 
-// -----------------
+// ----------------- Servo moteur et capteur -----------------
+Servo monServo; // Objet Servo pour contrôler le servomoteur
 
+int TRIG = 13; // Broche TRIG du capteur ultrason
+int ECHO = 12; // Broche ECHO du capteur ultrason
 
+long LECTURE_ECHO; // Temps mesuré par le capteur ultrason
+long CM;           // Distance en cm
+long CM_gauche;    // Distance mesurée à gauche
+long CM_droite;    // Distance mesurée à droite
 
-// ----------------- Variable
-#define speed_Max 100
-#define Turn90deg 500
-// -----------------
-
-
-
-// ----------------- Servo moteur / capteur
-Servo monServo;
-
-int TRIG = 13;
-int ECHO = 12;
-
-long LECTURE_ECHO;
-long CM;
-long CM_gauche;
-long CM_droite;
-
-// -----------------
-
+// Distances finales utilisées pour la logique du robot
 int distanceGauche = 0;
 int distanceDroite = 0;
 
+// ----------------- Fonction d'initialisation -----------------
 void setup() {
+  // Définir les broches moteurs comme sorties
+  pinMode(IN1, OUTPUT); 
+  pinMode(IN2, OUTPUT); 
+  pinMode(IN3, OUTPUT); 
+  pinMode(IN4, OUTPUT); 
 
-pinMode(IN1, OUTPUT); 
-pinMode(IN2, OUTPUT); 
-pinMode(IN3, OUTPUT); 
-pinMode(IN4, OUTPUT); 
-analogWrite(EN, speed_Max);
-monServo.attach(10);
-pinMode(TRIG, OUTPUT);
-pinMode(ECHO, INPUT);
-Serial.begin(9600);
+  // Activer le moteur avec la vitesse maximale
+  analogWrite(EN, speed_Max);
 
+  // Attacher le servomoteur à la broche 10
+  monServo.attach(10);
+
+  // Définir les broches du capteur ultrason
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+
+  // Démarrer la communication série pour afficher les distances
+  Serial.begin(9600);
 }
 
+// ----------------- Boucle principale -----------------
 void loop() {
+  // -------- Lecture du capteur ultrason --------
+  digitalWrite(TRIG, HIGH);          // Envoie l'impulsion ultrason
+  delayMicroseconds(1);              // Temps très court
+  digitalWrite(TRIG, LOW);           // Fin de l'impulsion
+  LECTURE_ECHO = pulseIn(ECHO, HIGH);// Lecture du signal de retour
+  CM = LECTURE_ECHO / 58;            // Conversion du temps en distance cm
+  Serial.println(CM);                // Affiche la distance mesurée
 
-// ----------------- Capteur
-  digitalWrite(TRIG, HIGH);
-  delayMicroseconds(1);
-  digitalWrite(TRIG, LOW);
-  LECTURE_ECHO = pulseIn(ECHO, HIGH);
-  CM = LECTURE_ECHO / 58;
-  Serial.println(CM);
-// -----------------
+  // -------- Contrôle du servomoteur --------
+  int position = 90;                  // Servomoteur centré
+  monServo.write(position);
 
-int position = 90; 
-monServo.write(position);
-moveStraight();
+  // -------- Mouvement du robot --------
+  moveStraight();                     // Avance tout droit
 
-if(CM <= 30){
+  // Si un obstacle est trop proche (< 30 cm)
+  if(CM <= 30){
+    stop();                           // Arrête le robot
+    verifygauchedroite();             // Vérifie les distances gauche/droite
+    delay(1000);                      // Pause avant de décider
 
-  stop();
-  verifygauchedroite();
-  delay(1000);
-
+    // Décision selon la distance la plus grande
     if(distanceGauche <= distanceDroite){
-
-      turnRight();
-      delay(350);
-
+      turnRight();                    // Tourne à droite
+      delay(350);                     // Temps approximatif pour tourner
     }
-    else if(distanceGauche >= distanceDroite){
-      turnLeft();
+    else if(distanceGauche > distanceDroite){
+      turnLeft();                     // Tourne à gauche
       delay(340);
     }
-  }
-  
+  }  
 }
 
-
-// ---------------- Fonctions enregistrée
-
-//----------------- Mouvements
+// ----------------- Fonctions de mouvements -----------------
 void moveStraight() {
+  // Active tous les moteurs pour avancer
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, HIGH);
 }
 
-
 void turnRight() {
+  // Logique pour tourner à droite (moteurs opposés)
   digitalWrite(IN1, HIGH);
   analogWrite(IN2, -1);
   analogWrite(IN3, -1);
@@ -104,13 +101,15 @@ void turnRight() {
 }
 
 void turnLeft() {
-   analogWrite(IN1, -1);
+  // Logique pour tourner à gauche (moteurs opposés)
+  analogWrite(IN1, -1);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, HIGH);
-   analogWrite(IN4, -1);
+  analogWrite(IN4, -1);
 }
 
 void moveBackward() {
+  // Recule le robot
   analogWrite(IN1, -1);
   analogWrite(IN2, -1);
   analogWrite(IN3, -1);
@@ -118,14 +117,16 @@ void moveBackward() {
 }
 
 void stop() {
+  // Arrête tous les moteurs
   digitalWrite(IN1, 0);
   digitalWrite(IN2, 0);
   digitalWrite(IN3, 0);
   digitalWrite(IN4, 0);
 }
-//----------------- 
 
+// ----------------- Fonctions pour le capteur ultrason -----------------
 void takemesuregauche() {
+  // Mesure la distance à gauche
   digitalWrite(TRIG, HIGH);
   delayMicroseconds(1);
   digitalWrite(TRIG, LOW);
@@ -133,10 +134,10 @@ void takemesuregauche() {
   CM_gauche = LECTURE_ECHO / 58;
   Serial.println(CM_gauche);
   return(CM_gauche);
-
 }
 
 void takemesuredroite() {
+  // Mesure la distance à droite
   digitalWrite(TRIG, HIGH);
   delayMicroseconds(1);
   digitalWrite(TRIG, LOW);
@@ -144,24 +145,21 @@ void takemesuredroite() {
   CM_droite = LECTURE_ECHO / 58;
   Serial.println(CM_droite);
   return(CM_droite);
-
 }
 
+// ----------------- Vérification des distances à gauche et à droite -----------------
 void verifygauchedroite() {
+  // Servomoteur orienté à gauche
+  int positionGauche = 180;
+  monServo.write(positionGauche);
+  delay(300);
+  takemesuregauche();             // Mesure distance gauche
+  distanceGauche = CM_gauche;
 
-    int positionGauche = 180;
-    monServo.write(positionGauche);
-    delay(300);
-    takemesuregauche( );
-    distanceGauche = CM_gauche;
-
-
-
-    int positionDroite = 0;
-    monServo.write(positionDroite);
-    delay(300);
-    takemesuredroite( );
-    distanceDroite = CM_droite;
-
-
-  }
+  // Servomoteur orienté à droite
+  int positionDroite = 0;
+  monServo.write(positionDroite);
+  delay(300);
+  takemesuredroite();             // Mesure distance droite
+  distanceDroite = CM_droite;
+}
